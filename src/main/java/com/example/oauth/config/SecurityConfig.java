b12c064b50f58b2ca63e8448978c20b639247bca
YuYoHan <zxzz8014@naver.com>
2023-06-30 23:58:54 +0900

    [Study] Oauth2

--- src/main/java/com/example/oauth/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.oauth.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
