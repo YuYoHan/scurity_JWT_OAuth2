0b3495bcce82382523487b1181dbbe36cc027b3f
YuYoHan <zxzz8014@naver.com>
2023-07-01 09:39:36 +0900

    [Study] entryPoint

--- src/main/java/com/example/oauth/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -4,7 +4,14 @@ package com.example.oauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -17,12 +24,21 @@ public class SecurityConfig {
                 .authenticated();
 
         http
-                .formLogin();
-
-
-        http
-                .apply(new CustomSecurityConfigurer().setFlag(false));
-
+//                .formLogin()
+//                .httpBasic()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new AuthenticationEntryPoint() {
+                            @Override
+                            public void commence(
+                                    HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    AuthenticationException authException)
+                                    throws IOException, ServletException {
+                                System.out.println("custom entryPoint");
+                            }
+                        }
+                );
         return http.build();
     }
 }
